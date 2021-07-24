d7a3dd5e907ad1f49b7e2b07c51d5b7588c8f0b2
John Smith <drzero123456@gmail.com>
2021-07-24 16:35:10 +0200

    Test commit

--- rangadoDownload.py
+++ rangadoDownload.py
@@ -8,9 +8,8 @@ from pathlib import Path
 from time import sleep
 import shutil
 
-"""
-rangadóMainPage = 'https://patria.rtvs.sk/clanky/rangado-derby?page='
-headers = headers = {
+RANGADO_MAIN_PAGE = 'https://patria.rtvs.sk/clanky/rangado-derby?page='
+HEADERS = headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'}
 
 
@@ -105,4 +104,3 @@ def rangadoDownloader():
 
 
 rangadoDownloader()
-"""
